d6138a61822350ceee01f32ad8cc873895aff7c2
Shawn Das <shawondasshachin@gmail.com>
2026-06-05 18:02:12 +0600

    6-5-2026

---
+++ Sorting and searching/Maximum Subarray Sum.cpp
@@ -0,0 +1,34 @@
+// Author: Shawn Das Shachin-->(shawn_das)
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+#define pb push_back
+#define mod 1000000007
+#define srt(v) sort(v.begin(),v.end())
+#define rsrt(v) sort(v.rbegin(),v.rend())
+#define OPTIMIZE_IO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define setbit(x) __builtin_popcount(x);
+#define printp(v) { for(auto &it : v) std::cout << it.first << " " << it.second << std::endl; }
+#define printarr(arr) { for(auto &it : arr) std::cout << it << " "; std::cout << std::endl; }
+
+void solve(){
+    ll n;
+    cin>>n;
+    vector<ll> v(n);
+    for(int i=0; i<n; i++)cin>>v[i];
+
+    ll ans=LONG_LONG_MIN,sum=0;
+    for(int i=0; i<n; i++){
+        sum+=v[i];
+        ans=max(ans,sum);
+        if(sum<0)sum=0;
+    }
+    cout<<ans<<endl;
+}
+int main() {
+    OPTIMIZE_IO;
+    solve();
+    return 0;
+}
